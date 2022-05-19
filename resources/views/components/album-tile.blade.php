89a90592cc7a956c2857a7b759b90e2610687dc2
Dan Stoakes <dan.stoakes8@gmail.com>
2022-05-19 23:37:27 +0100

    Create album tile component and some styling

---
+++ resources/views/components/album-tile.blade.php
@@ -0,0 +1,7 @@
+<div class="items-center flex flex-col">
+    <img class="object-cover aspect-square rounded w-full" src="https://picsum.photos/200/300" />
+    <div class="text-center mt-1.5">
+        <h2 class="text-md font-semibold line-clamp-1">{{ $caption }}</h2>
+        <h2 class="text-xs text-gray-600 font-thin leading-3">{{ $count }}</h2>
+    </div>
+</div>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,8 +8,24 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+                <div class="p-3 bg-white border-b border-gray-200">
+                    <div >
+                        <h1 class="w-full border-b border-gray-300 text-4xl text-bold">Albums</h1>
+                    </div>
+                    <div class="grid grid-cols-5 gap-10 p-6">
+                        <x-album-tile caption="Lorem ipsum dolor" count="20" />
+                        <x-album-tile caption="Lorem ipsum dolor" count="20" />
+                        <x-album-tile caption="Lorem ipsum dolor" count="20" />
+                        <x-album-tile caption="Lorem ipsum dolor" count="20" />
+                        <x-album-tile caption="Lorem ipsum dolor" count="20" />
+                        <x-album-tile caption="Lorem ipsum dolor" count="20" />
+                        <x-album-tile caption="Lorem ipsum dolor" count="20" />
+                        <x-album-tile caption="Lorem ipsum dolor" count="20" />
+                        <x-album-tile caption="Lorem ipsum dolor" count="20" />
+                        <x-album-tile caption="Lorem ipsum dolor" count="20" />
+                        <x-album-tile caption="Lorem ipsum dolor" count="20" />
+                        <x-album-tile caption="Lorem ipsum dolor" count="20" />
+                    </div>
                 </div>
             </div>
         </div>
