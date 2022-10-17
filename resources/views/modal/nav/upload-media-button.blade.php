c548974b1148f27393e5b1bf54d28f3c59fc7231
Dan Stoakes <dan.stoakes8@gmail.com>
2022-10-17 20:47:52 +0100

    Add old uncommitted changes

--- resources/views/modal/nav/upload-media-button.blade.php
+++ resources/views/modal/nav/upload-media-button.blade.php
@@ -19,5 +19,7 @@
     <svg xmlns="http://www.w3.org/2000/svg" class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
         <path stroke-linecap="round" stroke-linejoin="round" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12" />
     </svg>
-    <span class="flex-1 ml-3 whitespace-nowrap sm:block hidden">Upload</span>
+    @if (!isset($iconOnly))
+        <span class="flex-1 ml-3 whitespace-nowrap sm:block hidden">Upload</span>
+    @endif
 </a>
